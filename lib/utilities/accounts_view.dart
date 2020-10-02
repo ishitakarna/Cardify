57050f864798e670366299c809be34a466623257
ishitakarna <ishitakarna@gmail.com>
2020-10-03 00:11:06 +0530

    Create an onPressed functionality for the 'AddAccount' widget #5 Implemented

--- lib/utilities/accounts_view.dart
+++ lib/utilities/accounts_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cardify/constants.dart';
 
+import '../constants.dart';
+
 class AccountsView extends StatefulWidget {
   @override
   _AccountsViewState createState() => _AccountsViewState();
@@ -9,11 +11,142 @@ class AccountsView extends StatefulWidget {
 class _AccountsViewState extends State<AccountsView> {
   final myController = TextEditingController();
   final myController2 = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void dispose() {
     myController.dispose();
     myController2.dispose();
     super.dispose();
+  }
+
+  Color hexToColor(String code) {
+    return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
+  }
+
+  openDialogBox() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            title: Text('Add Account'),
+            content: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Stack(
+                overflow: Overflow.visible,
+                children: <Widget>[
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "Bank Name",
+                            fillColor: Colors.white,
+                            border: new OutlineInputBorder(
+                              borderSide: new BorderSide(),
+                            ),
+                            //fillColor: Colors.green
+                          ),
+                          keyboardType: TextInputType.text,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                        ),
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "Initial Balance",
+                            fillColor: Colors.white,
+                            border: new OutlineInputBorder(
+                              borderSide: new BorderSide(),
+                            ),
+                            //fillColor: Colors.green
+                          ),
+                          keyboardType: TextInputType.number,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                        ),
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "Account Holder Name",
+                            fillColor: Colors.white,
+                            border: new OutlineInputBorder(
+                              borderSide: new BorderSide(),
+                            ),
+                            //fillColor: Colors.green
+                          ),
+                          keyboardType: TextInputType.text,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                        ),
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "Account Type",
+                            fillColor: Colors.white,
+                            border: new OutlineInputBorder(
+                              borderSide: new BorderSide(),
+                            ),
+                            //fillColor: Colors.green
+                          ),
+                          keyboardType: TextInputType.text,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              SizedBox(
+                                width: 65,
+                                child: FlatButton(
+                                  color: Colors.transparent,
+                                  padding: EdgeInsets.all(0),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text(
+                                    'Cancel',
+                                    style: TextStyle(
+                                        color: kPrimaryColor, fontSize: 15),
+                                  ),
+                                ),
+                              ),
+                              // SizedBox(width: 3),
+                              SizedBox(
+                                width: 65,
+                                child: RaisedButton(
+                                  color: kPrimaryColor,
+                                  padding: EdgeInsets.all(0),
+                                  child: Text(
+                                    "Save",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15),
+                                  ),
+                                  onPressed: () {
+                                    // if (_formKey.currentState.validate()) {
+                                    //   _formKey.currentState.save();
+                                    // }
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 
   @override
@@ -37,7 +170,9 @@ class _AccountsViewState extends State<AccountsView> {
                     Icons.add_circle_outline,
                     size: 30,
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    openDialogBox();
+                  },
                 )
               ],
             ),
